bdd66896aa5107f9ffe91bc1c95de36eca8e201a
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2021-12-17 15:16:15 -0500

    Delete duo.sh
    Not needed and not relevant

--- duo.sh
+++
@@ -1,9 +0,0 @@
-cd ~/testDuo
-cd duo_client_python
-virtualenv .env
-source .env/bin/activate
-pip install --requirement requirements.txt
-pip install --requirement requirements-dev.txt
-python setup.py install
-cd examples
-python3 peake.py
